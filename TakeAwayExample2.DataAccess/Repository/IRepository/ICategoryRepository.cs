86078a4101434ebd13253089d9255aba5ccf0672
hugoemanuelpintopaulo <hugoemanuelpintopaulo@gmail.com>
2020-09-30 14:19:35 +0200

    Added The ICategoryRepository interface that implements the generic IRepository interface. Added the package microsoft aspnet core mvc rendering.

--- TakeAwayExample2.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ TakeAwayExample2.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TakeAwayExample2.Models;
 
 namespace TakeAwayExample2.DataAccess.Repository.IRepository
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(Category category);
     }
 }
