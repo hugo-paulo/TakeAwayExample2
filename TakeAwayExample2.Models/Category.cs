86acfe0114d8344196a895e2b5c100d825e2a0ee
hugoemanuelpintopaulo <hugoemanuelpintopaulo@gmail.com>
2020-09-30 11:26:13 +0200

    Added a Category Model.

---
+++ TakeAwayExample2.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TakeAwayExample2.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        //Tells EF this is a PK
+        //[Key]
+        public int CategoryID { get; set; }
+        
+        [Required]
+        [Display(Name = "Category Name")]
+        [MaxLength(100)]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
